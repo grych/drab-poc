398bdf028f0924d8bed07920fc327704a14d3dcc
Tomek Gryszkiewicz <grych@tg.pl>
2016-10-20 22:13:00 +0200

    cipher

--- mix.exs
+++ mix.exs
@@ -33,8 +33,8 @@ defmodule DrabPoc.Mixfile do
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"} 
-     # {:cipher, ">= 1.2.0"}
+     {:cowboy, "~> 1.0"},
+     {:cipher, ">= 1.2.0"}
     ]
   end
 end
