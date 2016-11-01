aa785acd602aa48eb53f23bdb8b68c8650c3207d
Tomek Gryszkiewicz <grych@tg.pl>
2016-11-01 17:52:30 +0100

    path for drab socket

--- lib/drab_poc/endpoint.ex
+++ lib/drab_poc/endpoint.ex
@@ -1,7 +1,7 @@
 defmodule DrabPoc.Endpoint do
   use Phoenix.Endpoint, otp_app: :drab_poc
 
-  socket "/drab/drab/socket", Drab.Socket
+  socket "/drab/socket", Drab.Socket
 
   # Serve at "/" the static files from "priv/static" directory.
   #
