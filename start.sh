14fd58b39ba751d65f71044909893f533597f11b
Tomek Gryszkiewicz <grych@tg.pl>
2016-11-03 10:11:28 +0100

    start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-export PORT=6503
-export MIX_ENV=prod
-elixir --detached -S mix phoenix.server
+PORT=6503 MIX_ENV=prod elixir --detached -S mix phoenix.server
+PORT=6504 MIX_ENV=prod elixir --detached -S mix phoenix.server
 
