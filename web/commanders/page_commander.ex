dd970fe773862d405af2bd6392c45484773713e8
Tomek Gryszkiewicz <grych@tg.pl>
2016-11-04 17:11:14 +0100

    api v2

--- web/commanders/page_commander.ex
+++ web/commanders/page_commander.ex
@@ -6,8 +6,12 @@ defmodule DrabPoc.PageCommander do
 
   # Drab Events
   def uppercase(socket, dom_sender) do
-    t = List.first(Drab.Query.val(socket, "#text_to_uppercase"))
-    Drab.Query.val(socket, "#text_to_uppercase", String.upcase(t))
+    t = List.first(Drab.Query.select(socket, "#text_to_uppercase", :val))
+    Drab.Query.update(socket, "#text_to_uppercase", :val, String.upcase(t))
+
+    # Drab.Query.update(socket, this(dom_sender), :addClass, "btn-danger")
+    # delete(socket, this(dom_sender), :class, "btn-primary")
+    update(socket, this(dom_sender), :class, "btn-primary", "btn-danger")
 
     {socket, dom_sender}
   end
@@ -17,47 +21,47 @@ defmodule DrabPoc.PageCommander do
     for i <- 1..steps do
       :timer.sleep(:rand.uniform(500)) # simulate real work
       socket 
-        |> attr(".progress-bar", "style", "width: #{i * 100 / steps}%")
-        |> html(".progress-bar", "#{Float.round(i * 100 / steps, 2)}%")
+        |> update(".progress-bar", :attr, "style", "width: #{i * 100 / steps}%")
+        |> update(".progress-bar", :html, "#{Float.round(i * 100 / steps, 2)}%")
     end
-    add_class(socket, ".progress-bar", "progress-bar-success")
+    insert(socket, ".progress-bar", :class, "progress-bar-success")
 
     {socket, dom_sender}
   end
 
   def run_async_tasks(socket, dom_sender) do
     socket 
-      |> change_class(".task", "label-success", "label-danger")
-      |> html("#async_task_status", "running")
+      |> update(".task", :class, "label-success", "label-danger")
+      |> update("#async_task_status", :text, "running")
 
     {_, begin_at_sec, begin_at_micsec } = :os.timestamp
     tasks = Enum.map(1..54, fn(i) -> Task.async(fn -> 
       :timer.sleep(:rand.uniform(4000)) # simulate real work
-      change_class(socket, ".task[data-task_id=#{i}]", "label-danger", "label-success")
+      update(socket, ".task[data-task_id=#{i}]", :class, "label-danger", "label-success")
       end)
     end)
     Enum.each(tasks, fn(task) -> Task.await(task) end)
     {_, end_at_sec, end_at_micsec } = :os.timestamp
     
-    html(socket, "#async_task_status", 
+    update(socket, "#async_task_status", :html, 
       "finished in #{((end_at_sec - begin_at_sec)*1000_000 + (end_at_micsec - begin_at_micsec))/1000_000} seconds")
 
     {socket, dom_sender}
   end
 
   def clicked_sleep_button(socket, dom_sender) do
-    prop(socket, this(dom_sender), "disabled", true)
+    update(socket, this(dom_sender), :prop, "disabled", true)
     :timer.sleep(dom_sender["data"]["sleep"] * 1000)
-    prop(socket, this(dom_sender), "disabled", false)
+    update(socket, this(dom_sender), :prop, "disabled", false)
   end
 
   def changed_input(socket, dom_sender) do
-    html(socket, "#display_placeholder", String.upcase(dom_sender["val"]))
+    update(socket, "#display_placeholder", :text, String.upcase(dom_sender["val"]))
   end
 
   # Drab Callbacks
   def page_loaded(socket) do
-    html(socket, "#display_placeholder", "Value set on the server side")
+    update(socket, "#display_placeholder", :html, "Value set on the server side")
     socket
   end
 end
