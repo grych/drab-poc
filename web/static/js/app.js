de7fe33a8e3769725a5990d713eb6a6c8956dbfc
Tomek Gryszkiewicz <grych@tg.pl>
2016-11-16 23:14:11 +0100

    typos and ortography, thanks Wudzia!

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,21 +1,13 @@
 import "phoenix_html"
-// import Drab from "drab"
-
-// var $ = require('jquery')
 
 global.bootstrap = require("bootstrap")
-// global.drab = require("drab")
-
-// import Drab from "web/static/js/drab"
 
 import hljs from "highlight.js"
 hljs.configure({
   languages: ['elixir', 'html', 'javascript']
   })
 hljs.initHighlightingOnLoad()
-// import "drab"
-// require('dupa').Dupa.run()
-// require('drab').Drab.run('aaa')
+
 
 
 
