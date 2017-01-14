0104a03cedfad262effea0fe643b2aea5cfdbf9b
Tomek Gryszkiewicz <grych@tg.pl>
2017-01-14 11:30:32 +0100

    0.1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule DrabPoc.Mixfile do
 
   def project do
     [app: :drab_poc,
-     version: "0.0.17",
+     version: "0.1.0",
      elixir: "~> 1.3",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
