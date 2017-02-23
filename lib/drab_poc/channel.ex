76cba566c01c21ac09c9acf27a3bde3798d1f8e9
Tomek Gryszkiewicz <grych@tg.pl>
2017-02-23 19:07:02 +0100

    macros and callbacks

---
+++ lib/drab_poc/channel.ex
@@ -0,0 +1,9 @@
+defmodule DrabPoc.Channel do
+  # use Phoenix.Channel
+  require Logger
+
+  # def join("mychannel:whatever", _, socket) do
+  #   Logger.debug("JOINED to mychannel")
+  #   {:ok, socket}
+  # end
+end
