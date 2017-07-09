87040c0f9cd7cec28c46c7fb569567699e23332a
Tomek Gryszkiewicz <grych@tg.pl>
2017-07-09 19:35:12 +0200

    live

--- web/commanders/live_commander.ex
+++ web/commanders/live_commander.ex
@@ -4,9 +4,14 @@ defmodule DrabPoc.LiveCommander do
 
   use Drab.Commander, modules: [Drab.Live]
 
-  def uppercase(socket, _sender) do
-    text = peek(socket, :text)
+  def uppercase(socket, sender) do
+    text = sender.params["text_to_uppercase"]
     poke socket, text: String.upcase(text)
+  end
+
+  def lowercase(socket, sender) do
+    text = sender.params["text_to_uppercase"]
+    poke socket, text: String.downcase(text)
   end
 
   def replace_list(socket, _sender) do
@@ -20,6 +25,18 @@ defmodule DrabPoc.LiveCommander do
   def add_to_list(socket, _sender) do
     users = Drab.Live.peek(socket, :users)
     Drab.Live.poke socket, users: users ++ ["Hegemon"]
+  end
+
+  def clicked_sleep_button(socket, sender) do
+    button_no = sender["data"]["sleep"] |> String.to_integer()
+    
+    cl = peek socket, :sleep_button_classes
+    poke socket, sleep_button_classes: %{cl | button_no => "btn-danger"}
+
+    Process.sleep(button_no * 1000)
+    
+    cl = peek socket, :sleep_button_classes
+    poke socket, sleep_button_classes: %{cl | button_no => "btn-primary"}    
   end
 
   def changed_label(socket, sender) do
--- web/controllers/live_controller.ex
+++ web/controllers/live_controller.ex
@@ -5,7 +5,7 @@ defmodule DrabPoc.LiveController do
   def index(conn, _params) do
     render conn, "index.html", text: "uppercase me",
       users: ["Dżesika", "Brajanek", "Zdzichu"], title: "Users List",
+      sleep_button_classes: %{1 => "btn-primary", 2 => "btn-primary", 3 => "btn-primary"},
       label: "default", button_height: 30
-
   end
 end
