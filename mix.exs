f62452a3944b27a4593212827bba0f68f2aef725
Tomek Gryszkiewicz <grych@tg.pl>
2017-10-24 19:03:59 +0200

    0.6.0 pre release

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule DrabPoc.Mixfile do
 
   def project do
     [app: :drab_poc,
-     version: "0.5.5",
+     version: "0.6.0",
      elixir: "~> 1.4",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
@@ -29,16 +29,16 @@ defmodule DrabPoc.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.2"},
+    [{:phoenix, "~> 1.3"},
      {:phoenix_pubsub, "~> 1.0"},
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
      {:earmark, "~> 1.0.3"},
-     # {:drab, "~> 0.5.4"},
+     {:drab, "~> 0.6.0-pre.1"},
      # {:drab, in_umbrella: true},
-     {:drab, path: "../drab"},
+     # {:drab, path: "../drab"},
      {:logger_file_backend, "~> 0.0.9"},
      {:sentix, "~> 1.0"},
      {:ip2country, "~> 1.0"},
