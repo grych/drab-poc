fc9dbc60220e340c9600a7b8a244167d3cce4b3b
Tomek Gryszkiewicz <grych@tg.pl>
2017-11-01 20:43:54 +0100

    v0.6.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule DrabPoc.Mixfile do
 
   def project do
     [app: :drab_poc,
-     version: "0.6.0",
+     version: "0.6.1",
      elixir: "~> 1.4",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
@@ -36,8 +36,8 @@ defmodule DrabPoc.Mixfile do
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
      {:earmark, "~> 1.0.3"},
-     # {:drab, "~> 0.6.0"},
-     {:drab, github: "grych/drab"},
+     {:drab, "~> 0.6.1"},
+     # {:drab, github: "grych/drab"},
      # {:drab, path: "../drab"},
      {:logger_file_backend, "~> 0.0.9"},
      {:sentix, "~> 1.0"},
