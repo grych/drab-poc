c56530333bc753c28ba289f23c6e368cc63599c1
Tomek Gryszkiewicz <grych@tg.pl>
2018-11-28 13:25:24 +0100

    fixes

--- web/views/docs_view.ex
+++ web/views/docs_view.ex
@@ -3,12 +3,12 @@ defmodule DrabPoc.DocsView do
 
   def moduledoc(module) do
     {_line, md} = Code.get_docs(module, :moduledoc)
-    "#{Earmark.as_html(md, %Earmark.Options{code_class_prefix: "elixir"})}"
+    "#{Earmark.to_html(md, %Earmark.Options{code_class_prefix: "elixir"})}"
   end
 
   # function - a tuple of {funcion_name, arity}
   def funcdoc(module, function) do
     {_function, _line, _def, _arguments, doc} = Code.get_docs(module, :docs) |> Enum.find(fn(x) -> elem(x, 0) == function end)
-    Earmark.as_html(doc)
+    Earmark.to_html(doc)
   end
 end
