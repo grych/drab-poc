7bcc5827cc1d6653858a2d98dfdffe002feb39e0
Tomek Gryszkiewicz <grych@tg.pl>
2018-11-28 12:00:27 +0100

    fixes

--- mix.exs
+++ mix.exs
@@ -28,19 +28,20 @@ defmodule DrabPoc.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.3"},
-     {:phoenix_pubsub, "~> 1.0"},
-     {:phoenix_html, "~> 2.6"},
-     {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0 or ~> 2.2.2 or ~> 2.3"},
-     {:earmark, "~> 1.0.3"},
+    [
+      {:phoenix, "~> 1.4.0"},
+      {:phoenix_pubsub, "~> 1.1"},
+      {:phoenix_html, "~> 2.11"},
+      {:phoenix_live_reload, "~> 1.2", only: :dev},
+      {:gettext, "~> 0.11"},
+      {:jason, "~> 1.0"},
+      {:plug_cowboy, "~> 2.0"},
      #{:drab, "~> 0.7.4"},
      {:drab, path: "../drab"},
      {:logger_file_backend, "~> 0.0.9"},
      {:sentix, "~> 1.0"},
      {:ip2country, "~> 1.0"},
-     {:jason, "~> 1.0"}
+     {:earmark, "~> 1.3.0"}
      # {:appsignal, "~> 1.0"}
     ]
   end
