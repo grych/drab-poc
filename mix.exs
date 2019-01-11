ecca7ed51be0d54aacb946bf236524d021a8b101
Tomek Gryszkiewicz <grych@tg.pl>
2019-01-11 16:50:24 +0100

    0.10.1

--- mix.exs
+++ mix.exs
@@ -31,7 +31,7 @@ defmodule DrabPoc.Mixfile do
     [
       {:phoenix, "~> 1.3.0"},
      {:phoenix_pubsub, "~> 1.0.0"},
-     {:phoenix_html, "~> 2.6.0"},
+     {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0.0", only: :dev},
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
